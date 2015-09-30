3e562742e96e05c6751022dcf45a3d9f2054e9b1
GLR <gavin@beckshybrids.com>
2015-09-30 09:59:41 -0400

    Add login page

---
+++ app/login/login.js
@@ -0,0 +1,15 @@
+(function(){
+  angular.module('notely.login', [
+    'ui.router'
+  ])
+    .config(loginConfig);
+
+  loginConfig['$inject'] = ['$stateProvider'];
+  function loginConfig($stateProvider) {
+    $stateProvider
+      .state('login', {
+        url: '/login',
+        templateUrl: 'login/login.html'
+      });
+  }
+})();
